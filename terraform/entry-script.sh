c784a619d71250b894f46462cc9173a0d2170538
Ravi kumar <87536380+ravimanchi0321@users.noreply.github.com>
2023-09-24 11:27:35 +0530

    Update entry-script.sh

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install -y docker
+sudo apt-get update && sudo apt install docker.io -y
 sudo systemctl start docker
 sudo usermod -aG docker $USER
 sudo chmod 666 /var/run/docker.sock
