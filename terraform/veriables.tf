ae4d867cebc088f743a24b118b1850c0a1de7c13
Ravi kumar <87536380+ravimanchi0321@users.noreply.github.com>
2023-09-24 09:17:52 +0530

    Update veriables.tf

--- terraform/veriables.tf
+++ terraform/veriables.tf
@@ -11,16 +11,40 @@ variable "ami" {
   default = "ami-053b0d53c279acc90"
   
 }
-/*
+
 variable "instance_type" {
   type = string
-  description = "t2.micro"
+  default = "t2.micro"
 }
- */ 
+
 
 variable "key_name" {
     type = string
     default = "k8s"
     description = "key name"
   
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "The IP range to use for the VPC"
+  default     = "10.0.0.0/16"
+}
+ 
+variable "availbility_zone" {
+  type = string
+  description = "AZs to create subnets into"
+  default = "us-east-1"
+}
+
+variable "public_subnet" {
+  type = string
+  description = "subnets to create for public network traffic, one per AZ"
+  default = "10.0.1.0/24"
+}
+ 
+variable "private_subnet" {
+  type = string
+  description = "subnets to create for private network traffic, one per AZ"
+  default = "10.0.2.0/24"
 }
