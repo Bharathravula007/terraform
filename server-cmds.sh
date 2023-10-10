c63b970396c844282d0627df6161f3cade4dd4f9
Ravi kumar <87536380+ravimanchi0321@users.noreply.github.com>
2023-10-10 15:03:39 +0530

    Delete server-cmds.sh

--- server-cmds.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-export DOCKER_USER=$1
-export DOCKER_PWD=$2
-echo $DOCKER_PWD | docker login -u $DOCKER_USER --password-stdin
-docker pull ravikumarmanchi/terraform:latest
-docker images
-docker run -itd -p 80:80 --name terraform ravikumarmanchi/terraform:latest
-docker ps
